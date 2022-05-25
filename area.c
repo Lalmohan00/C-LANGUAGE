cf5b57b3d3e549609fa75afd5d9937cfa535c0ef
LALMOHAN PAL <87718242+Lalmohan00@users.noreply.github.com>
2022-05-25 20:43:36 +0530

    Add files via upload

---
+++ area.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+	int l,b,area;
+	printf("enter the leanth =");
+	scanf("%d",&l);
+	printf("enter the breath =");
+	scanf("%d",&b);
+	
+	area=(l*b);
+	printf("area of perimeter =%d",area);
+	
+	return 0;
+}
